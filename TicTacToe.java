007eaacb9877bcf7f4befef36afa0288f233c088
psnakhwa <psnakhwa@ncsu.edu>
2017-03-16 17:18:36 -0400

    initialised the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,1 +1,34 @@
 import java.util.*;
+
+public class TicTacToe{
+
+	private char[][] board;
+	private char currentPlayerMark;
+
+	// Constructor Call
+	public TicTacToe(){
+		board = new char[3][3];
+		currentPlayerMark = 'x';
+		initializeBoard();
+	}
+
+	// Initialize the board
+	public void initializeBoard(){
+
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				board[i][j] = '-';
+			}
+		}
+	}
+
+	public void printBoard(){
+		System.out.println("######################");
+
+		for(int i=0; i<3;i++){
+			System.out.print("| ");
+		}
+		System.out.println();
+		System.out.println("######################");
+	}
+}
